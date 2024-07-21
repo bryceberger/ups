399cc1723d111e2e4dc0b15343fce86f3fb25045
Bryce Berger <bryce.z.berger@gmail.com>
2024-07-21 03:38:23 -0400

    refactor patching to use internal iteration

--- ups/src/lib.rs
+++ ups/src/lib.rs
@@ -6,15 +6,12 @@ pub enum Error {
     MissingHeader,
     #[error("Input patch malformed")]
     MalformedPatch,
-    #[error("CRC mismatch ({0:?})")]
-    CrcMismatch(CrcKind),
-}
-
-#[derive(Debug)]
-pub enum CrcKind {
-    Original,
-    Patch,
-    Combined,
+    #[error("CRC mismatch in original file")]
+    CrcMismatchOriginal,
+    #[error("CRC mismatch in patch file")]
+    CrcMismatchPatch,
+    #[error("CRC mismatch in output file")]
+    CrcMismatchTarget,
 }
 
 #[derive(Default)]
@@ -26,92 +23,71 @@ pub fn apply_patch(source: Vec<u8>, patch: &[u8]) -> Result<Vec<u8>, Error> {
     apply_patch_with(Default::default(), source, patch)
 }
 
-pub fn apply_patch_with(
-    options: Options,
-    mut source: Vec<u8>,
-    patch: &[u8],
-) -> Result<Vec<u8>, Error> {
-    let (p, mut patch_offset) = parse_patch(patch)?;
+pub fn apply_patch_with(options: Options, source: Vec<u8>, patch: &[u8]) -> Result<Vec<u8>, Error> {
+    let (p, it) = parse_patch(patch)?;
 
     if !options.skip_crc {
-        verify_crc(&source, p.source_crc).map_err(|_| Error::CrcMismatch(CrcKind::Original))?;
-        verify_crc(&patch[..patch.len() - 4], p.patch_crc)
-            .map_err(|_| Error::CrcMismatch(CrcKind::Patch))?;
+        verify_crc(&source, p.source_crc).map_err(|_| Error::CrcMismatchOriginal)?;
+        let patch_crc_data = &patch[..patch.len() - 4];
+        verify_crc(patch_crc_data, p.patch_crc).map_err(|_| Error::CrcMismatchPatch)?;
     }
 
-    source.resize(p.source_size.max(p.target_size) as _, 0);
+    let mut target = source;
+    target.resize(p.source_size.max(p.target_size) as _, 0);
 
-    let mut write_offset = 0;
-    while patch_offset < patch.len() - 12 {
-        let (consumed, skip_len) =
-            read_vuint(&patch[patch_offset..]).ok_or(Error::MalformedPatch)?;
-        patch_offset += consumed;
-        write_offset += skip_len as usize;
-
-        let w = write_offset;
-        for x in &patch[patch_offset..] {
-            source[write_offset] ^= x;
-            write_offset += 1;
-            if *x == 0 {
-                break;
-            }
-        }
-        patch_offset += write_offset - w;
-    }
+    it.fold(0, |write_offset, it| {
+        xor_slice(&mut target[write_offset + it.take..], it.xor);
+        write_offset + it.take + it.xor.len()
+    });
 
     if !options.skip_crc {
-        verify_crc(&source, p.target_crc).map_err(|_| Error::CrcMismatch(CrcKind::Combined))?;
+        verify_crc(&target, p.target_crc).map_err(|_| Error::CrcMismatchTarget)?;
     }
 
-    Ok(source)
+    Ok(target)
 }
 
-struct UpsPatch {
-    source_size: u64,
-    target_size: u64,
-    source_crc: u32,
-    target_crc: u32,
-    patch_crc: u32,
+pub struct UpsPatch {
+    pub source_size: usize,
+    pub target_size: usize,
+    pub source_crc: u32,
+    pub target_crc: u32,
+    pub patch_crc: u32,
 }
 
-/// -> (patch, consumed)
-fn parse_patch(patch: &[u8]) -> Result<(UpsPatch, usize), Error> {
+pub fn parse_patch(patch: &[u8]) -> Result<(UpsPatch, UpsSectionIter<'_>), Error> {
     let Some(b"UPS1") = patch.get(..4) else {
         return Err(Error::MissingHeader);
     };
 
-    let mut offset = 4;
-    let (consumed, source_size) = read_vuint(&patch[offset..]).ok_or(Error::MalformedPatch)?;
-    offset += consumed;
-    let (consumed, target_size) = read_vuint(&patch[offset..]).ok_or(Error::MalformedPatch)?;
-    offset += consumed;
+    let (s_used, source_size) = read_vuint(&patch[4..]).ok_or(Error::MalformedPatch)?;
+    let (t_used, target_size) = read_vuint(&patch[4 + s_used..]).ok_or(Error::MalformedPatch)?;
+    let offset = 4 + s_used + t_used;
 
     if patch.len() < offset + 12 {
         return Err(Error::MalformedPatch);
     }
 
     let get_crc = |o| u32::from_le_bytes(patch[o..o + 4].try_into().unwrap());
-    let crc_offset = patch.len() - 12;
-    let source_crc = get_crc(crc_offset);
-    let target_crc = get_crc(crc_offset + 4);
-    let patch_crc = get_crc(crc_offset + 8);
     let ups_patch = UpsPatch {
         source_size,
         target_size,
-        source_crc,
-        target_crc,
-        patch_crc,
+        source_crc: get_crc(patch.len() - 12),
+        target_crc: get_crc(patch.len() - 8),
+        patch_crc: get_crc(patch.len() - 4),
     };
-    Ok((ups_patch, offset))
+    let it = UpsSectionIter::new(&patch[offset..patch.len() - 12]);
+    Ok((ups_patch, it))
 }
 
 /// -> (consumed bytes, value)
-fn read_vuint(input: &[u8]) -> Option<(usize, u64)> {
+fn read_vuint(input: &[u8]) -> Option<(usize, usize)> {
     let val = input.iter().enumerate().try_fold(0, |acc, (idx, x)| {
+        let shift = idx * 7;
         if x & 0x80 != 0 {
-            ControlFlow::Break((idx + 1, acc + ((*x as u64 & 0x7f) << idx * 7)))
+            ControlFlow::Break((idx + 1, acc + ((*x as usize & 0x7f) << shift)))
         } else {
-            ControlFlow::Continue(acc + ((*x as u64 | 0x80) << idx * 7))
+            ControlFlow::Continue(acc + ((*x as usize | 0x80) << shift))
         }
     });
     match val {
@@ -125,4 +101,48 @@ fn verify_crc(data: &[u8], expected: u32) -> Result<(), ()> {
     (crc::Crc::<u32>::new(&ALG).checksum(data) == expected)
         .then_some(())
         .ok_or(())
+}
+
+fn xor_slice(left: &mut [u8], right: &[u8]) {
+    left.iter_mut().zip(right).for_each(|(l, r)| *l ^= r);
+}
+
+pub struct UpsSectionIter<'d> {
+    data: &'d [u8],
+    offset: usize,
+}
+
+pub struct UpsSection<'d> {
+    /// Number of bytes to take directly from the source.
+    pub take: usize,
+    /// Take `xor.len()` from the source, then elementwise xor.
+    pub xor: &'d [u8],
+}
+
+impl<'d> UpsSectionIter<'d> {
+    /// data _without_ header and footer (i.e. no "UPS1", no file size, no crc)
+    const fn new(data: &'d [u8]) -> Self {
+        Self { data, offset: 0 }
+    }
+}
+
+impl<'d> Iterator for UpsSectionIter<'d> {
+    type Item = UpsSection<'d>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let Self { data, offset } = self;
+        let (consumed, take) = read_vuint(&data[*offset..])?;
+        *offset += consumed;
+
+        let begin = *offset;
+        while *offset < data.len() {
+            *offset += 1;
+            if data[*offset - 1] == 0 {
+                let xor = &data[begin..*offset];
+                return Some(UpsSection { take, xor });
+            }
+        }
+
+        None
+    }
 }
