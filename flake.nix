e2658df46e0da2a8a980d84c24e4f063e9a3839f
Bryce Berger <bryce.z.berger@gmail.com>
2024-07-21 01:01:57 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    fenix = {
+      url = "github:nix-community/fenix/monthly";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    toolchain = fenix.packages.${system}.complete.toolchain;
+  in {
+    devShell.${system} = pkgs.mkShell {
+      packages = with pkgs; [
+        toolchain
+        mold
+      ];
+    };
+  };
+}
