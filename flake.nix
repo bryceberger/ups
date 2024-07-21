26053e81a2f349e347c794b81747ebc447aca199
Bryce Berger <bryce.z.berger@gmail.com>
2024-07-21 04:15:11 -0400

    add naersk to flake

--- flake.nix
+++ flake.nix
@@ -5,22 +5,36 @@
       url = "github:nix-community/fenix/monthly";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
     fenix,
-  }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-    toolchain = fenix.packages.${system}.complete.toolchain;
-  in {
-    devShell.${system} = pkgs.mkShell {
-      packages = with pkgs; [
-        toolchain
-        mold
-      ];
-    };
-  };
+    naersk,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      toolchain = fenix.packages.${system}.complete.toolchain;
+      naersk' = (pkgs.callPackage naersk {}).override {
+        cargo = toolchain;
+        rustc = toolchain;
+      };
+
+      ups-apply = naersk'.buildPackage {src = ./.;};
+    in {
+      packages = {
+        inherit ups-apply;
+        default = ups-apply;
+      };
+      devShell = pkgs.mkShell {
+        packages = [toolchain];
+      };
+    });
 }
